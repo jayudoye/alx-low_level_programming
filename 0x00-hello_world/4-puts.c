5779febb32472ddf7f33c272e8948f6a30c0392e
jayudoye <joeludoye@gmail.com>
2023-02-16 17:31:05 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - The main is void
+ *
+ * Return: The return is 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
