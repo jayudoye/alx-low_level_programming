0fd55b13fa1d133571dccc760d3d64670b6e6afc
jayudoye <joeludoye@gmail.com>
2023-02-16 17:44:49 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is a C program that uses printf
+ *
+ * Return: The return will be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
