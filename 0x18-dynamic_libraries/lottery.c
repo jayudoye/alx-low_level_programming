f49ba5d01303967afd32e5a149d8893139b3bc30
jayudoye <joeludoye@gmail.com>
2024-03-25 21:37:47 -0700

    Jackpot

---
+++ 0x18-dynamic_libraries/lottery.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int printf(const char *format, ...)
+{
+	puts("9 8 10 24 75 +9\n");
+	puts("Congratulations, you win the Jackpot!\n");
+	exit(EXIT_SUCCESS);
+}
